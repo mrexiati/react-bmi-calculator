421c4051f41792859064762a4667b340765b5103
RishatMamat <rishatmamat@RishatMamats-MacBook-Pro.local>
2022-04-20 20:51:49 -0600

    Add css style bmi form

--- src/components/BmiForm/BmiForm.jsx
+++ src/components/BmiForm/BmiForm.jsx
@@ -25,7 +25,55 @@ const BmiForm = ({ change }) => {
         });
     };
 
+    const handleSubmit = () => {
+        change(state);
+        setState(initialValue);
+    }
 
+    return(
+        <>
+            <div className="row">
+                <div className="col m6 s12">
+                    <label htmlFor="weight">Weight (in kg)</label>
+                    <input
+                    id="weight"
+                    name="weight"
+                    type="number"
+                    min="1"
+                    max="999"
+                    placeholder="Enter weight in kg"
+                    value={state.weight}
+                    onChange={handleChange}
+                    />
+                </div>
+
+                <div className="col m6 s12">
+                    <label htmlFor="weight">Height (in cm)</label>
+                    <input
+                    id="height"
+                    name="height"
+                    type="number"
+                    min="1"
+                    max="999"
+                    placeholder="Enter height in cm"
+                    value={state.height}
+                    onChange={handleChange}
+                    />
+                </div>
+            </div>
+            <div className="center">
+                <button 
+                    id="bmi-btn"
+                    className="calculate-btn"
+                    type="button"
+                    disabled={state.weight === '' || state.height === '' || state.weight < 0 || state.height < 0}
+                    onClick={handleSubmit}
+                >
+                    Calculate BMI
+                </button>
+            </div>
+        </>
+    );
 };
 
 BmiForm.propTypes = {
