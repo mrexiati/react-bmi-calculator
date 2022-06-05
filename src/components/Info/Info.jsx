61332521b79d6cc6957bf0d441b7fbbf1e546328
RishatMamat <reshatma@gmail.com>
2022-06-05 16:50:48 -0600

    Final

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,39 +1,40 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import '../App/App.css'
 
-const Info = ({ weight, height, id, date, bmi, deleteCard}) => {
-    const handleDelete = () => {
-        deleteCard(id);
-    }
+const Info = ({ weight, height, id, date, bmi, deleteCard }) => {
+  const handleDelete = () => {
+    deleteCard(id);
+  };
 
-    return (
-        <div className="col m6 s12">
-            <div className="card">
-                <div className="card-content">
-                    <span className="card-title" data-testid="bmi-value">
-                        BMI: {bmi}
-                    </span>
-                    <div className="card-data">
-                        <span data-testid="weight-value">Weight: {weight} kg</span>
-                        <span data-testid="height-value">Height: {height} cm</span>
-                        <span data-testid="date-value">Date: {date}</span>
-                    </div>
+  return (
+    <div className="col m6 s12">
+      <div className="card">
+        <div className="card-content">
+          <span className="card-title" data-test="bmi">
+            BMI: {bmi}
+          </span>
+          <div className="card-data">
+            <span data-test="weight">Weight: {weight} kg</span>
+            <span data-test="height">Height: {height} cm</span>
+            <span data-test="date">Date: {date}</span>
+          </div>
 
-                    <button className="delete-btn" onClick={handleDelete}>X</button>
-                </div>
-           </div>
+          <button className="delete-btn" onClick={handleDelete}>
+            X
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-Info.PropTypes = {
-    weight: PropTypes.string,
-    height: PropTypes.string,
-    id: PropTypes.string,
-    date: PropTypes.string,
-    bmi: PropTypes.string,
-    deleteCard: PropTypes.func
+Info.propTypes = {
+  weight: PropTypes.string,
+  height: PropTypes.string,
+  id: PropTypes.string,
+  date: PropTypes.string,
+  bmi: PropTypes.string,
+  deleteCard: PropTypes.func
 };
 
 export default Info;
--- src/helpers/localStorage.js
+++ src/helpers/localStorage.js
@@ -1,23 +1,19 @@
 export const getData = (key) => {
-    if(!localStorage) {
-        return;
-    }
+	if (!localStorage) return;
 
-    try {
-        return JSON.parse(localStorage.getItem(key));
-    } catch (err) {
-        console.error(`Error getting item ${key} from localStorage`, err);
-    }
+	try {
+		return JSON.parse(localStorage.getItem(key));
+	} catch (err) {
+		console.error(`Error getting item ${key} from localStorage`, err);
+	}
 };
 
 export const storeData = (key, item) => {
-    if(!localStorage) {
-        return;
-    }
+	if (!localStorage) return;
 
-    try {
-        return localStorage.setItem(key, JSON.stringify(item));
-    } catch (err) {
-        console.error(`Error setting item ${key} to localStorage`, err);
-    }
-}
+	try {
+		return localStorage.setItem(key, JSON.stringify(item));
+	} catch (err) {
+		console.error(`Error storing item ${key} to localStorage`, err);
+	}
+};
